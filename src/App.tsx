1399231c9d6c4d04b6fe1f47c14dd7e28cecb5e0
Mahammad Rafi Shaik <shaikmahammadrafi484@gmail.com>
2026-08-09 15:00:19 +0530

    feat: add experience section to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
        <Hero />
        <About />
        <Skills />
+       <Experience />
       </main>
     </>
   );
